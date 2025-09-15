7ba4e4d4df29aa47eabb84b3a69d9b79dec8d95a
ErikaArci99 <erikaarci1999@outlook.it>
2025-09-15 20:12:19 +0200

    Added private and public method in homecontroller

--- src/main/java/org/lessons/java/best_of_the_year/controllers/HomeController.java
+++ src/main/java/org/lessons/java/best_of_the_year/controllers/HomeController.java
@@ -1,8 +1,12 @@
 package org.lessons.java.best_of_the_year.controllers;
 
+import java.util.List;
+import org.lessons.java.best_of_the_year.models.Movie;
+import org.lessons.java.best_of_the_year.models.Song;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import java.util.Arrays;
 
 @Controller
 public class HomeController {
@@ -15,4 +19,31 @@ public class HomeController {
         return "index";
     }
 
+    // metodi privati
+    private List<Movie> getBestMovies() {
+        return Arrays.asList(
+                new Movie("Inception", 1),
+                new Movie("Titanic", 2),
+                new Movie("Avatar", 3));
+    }
+
+    private List<Song> getBestSongs() {
+        return Arrays.asList(
+                new Song("Imagine", 1),
+                new Song("Hey Jude", 2),
+                new Song("Shape of You", 3));
+    }
+
+    // Metodi pubblici
+    @GetMapping("/movies")
+    public String movies(Model model) {
+        model.addAttribute("list", getBestMovies());
+        return "movies";
+    }
+
+    @GetMapping("/songs")
+    public String songs(Model model) {
+        model.addAttribute("list", getBestSongs());
+        return "songs";
+    }
 }
